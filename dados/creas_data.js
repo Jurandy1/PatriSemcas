d00a4cb61056c7740f8873386ff520baf71917d4
Jurandy1 <jujuba100054@gmail.com>
2025-06-27 16:26:33 -0300

    Delete dados/creas_data.js

--- dados/creas_data.js
+++
@@ -1,4 +0,0 @@
-[
-  {"id":"cre_1","description":"Ar-Condicionado 12.000 BTU","unit_condition":"CREAS Centro","quantity":2,"state":"Bom","donation":"Não"},
-  {"id":"cre_2","description":"Mesa para Escritório","unit_condition":"CREAS Bacanga","quantity":5,"state":"Novo","donation":"Não"}
-]
