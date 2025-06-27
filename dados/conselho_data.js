186ccf65c28ae7a113f69ff9c915a1bcbc21ad59
Jurandy1 <jujuba100054@gmail.com>
2025-06-27 15:57:10 -0300

    Add files via upload

---
+++ dados/conselho_data.js
@@ -0,0 +1,4 @@
+[
+  {"id":"con_1","description":"Arquivo em Aço","unit_condition":"Conselho Tutelar - Área 1","quantity":8,"state":"Novo","donation":"Não"},
+  {"id":"con_2","description":"Bebedouro de Coluna","unit_condition":"Conselho Tutelar - Área 2","quantity":3,"state":"Avariado","donation":"Sim"}
+]
---
+++ dados/cras_data.js
@@ -0,0 +1,4 @@
+[
+  {idcra_1,descriptionCadeira de Plástico,unit_conditionCRAS João de Deus,quantity50,stateBom,donationSim},
+  {idcra_2,descriptionComputador Positivo,unit_conditionCRAS Cidade Operária,quantity10,stateRegular,donationNão}
+]
