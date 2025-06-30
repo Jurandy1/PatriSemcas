29bd38fe50406ef2a1546f4818167f40641e7049
Jurandy1 <jujuba100054@gmail.com>
2025-06-30 11:51:04 -0300

    Delete dados directory

--- dados/cras_data.js
+++
@@ -1,4 +0,0 @@
-[
-  {idcra_1,descriptionCadeira de Plástico,unit_conditionCRAS João de Deus,quantity50,stateBom,donationSim},
-  {idcra_2,descriptionComputador Positivo,unit_conditionCRAS Cidade Operária,quantity10,stateRegular,donationNão}
-]
